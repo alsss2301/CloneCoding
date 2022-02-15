7dd8eb9b0a74a98e78ded10b338c2fba72b154d6
Minseo Jeong <jeongminseo999@gmail.com>
2022-02-16 04:40:10 +0900

    220215

---
+++ cointracker/App.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import CoinTracker from './CoinTracker';
+
+function App() {
+  return <CoinTracker />;
+}
+
+export default App;
---
+++ cointracker/CoinTracker.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+function CoinTracker() {
+    const [loading, setLoading] = useState(true);
+    const [coins,setCoins] = useState([]);
+    const [cost, setCost] = useState("");
+    const [select,setSelect] = useState(0);
+
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+        .then((response) => response.json()) // 코인 받아오기
+        .then((json) => {
+            setCoins(json); // 코인데이터를 state에 넣기
+            setLoading(false); //코인얻기가 끝나면 false로 바꿔서 로딩중 없애기
+        });
+    }, []);
+
+    const onSelectChange = (e) => setSelect(e.target.value);
+    const inputHandle = (e) => setCost(e.target.value);
+    
+    return (
+        <div>
+            <h1>Coin Tracker! {loading ? "" : `(${coins.length})`}</h1>
+            <h2><input onChange={inputHandle} placeholder='금액을 입력하세요.' value={cost}/></h2>
+            {loading ? (
+                <strong>Loading...</strong>
+            ) : (
+            <select onChange={onSelectChange}>
+                {coins.map((coin, id) => (
+                  <option key={id} value={coin. quotes.USD.price}>
+                    {coin.name} ({coin.symbol}) : {coin.quotes.USD.price}
+                  </option>
+                ))}
+            </select>
+            )}
+            <h3>{(cost / select) > 0 ? (cost/select) : `...`}</h3>  
+        </div> // coin은 coins 배열안에 들어 있는 coin들(객체)
+    );
+}
+
+export default CoinTracker;
