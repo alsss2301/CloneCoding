0fcc930ec3d71d0f9e5d3ef4bc14b3f172105901
Minseo Jeong <jeongminseo999@gmail.com>
2022-02-25 19:07:37 +0900

    axios 활용 전문

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/routes/Home';
 import Detail from './components/routes/Detail';
--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -6,24 +6,34 @@ import styles from './Home.module.css'; //같은 폴더 안에 있을때는 ./
 function Home() {
     const [loading, setLoading] = useState(true);
     const [movies,setMovies] = useState([]);
-    const getMovies = async () => {
-      const response = await axios.get(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      );
-      const json = await response.json();
-      setMovies(json.data.movies);
-      setLoading(false);
-    };
+    const [error, setError] = useState(null);
+  
     useEffect(() => {
+      const getMovies = async () => {
+        try{
+          setError(null);
+          const response = await axios.get(
+            'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year'
+          );
+          setMovies(response.data.data.movies);
+        } catch (e) {
+          setError(e);
+        }
+        setLoading(false);
+      };
+
       getMovies();
     }, []);
+
+    if (error) return <div>에러가 발생했습니다</div>;
+
     return (
       <div className={styles.container}> 
         {loading ? (
           <h1>Loading...</h1>
         ) : (
           <div className={styles.movies}>
-            {movies.map((movie)=> (
+            {movies && movies.map((movie) => (
               <Movie 
                 key={movie.id}
                 id={movie.id}
